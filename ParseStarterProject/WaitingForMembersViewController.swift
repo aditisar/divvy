265ded3915180eb92b31404061cbb2da12e1fcb2
Aditi Sarkar <aditi0821@gmail.com>
2015-12-05 21:22:44 -0500

    wait screen table view is working

--- ParseStarterProject/WaitingForMembersViewController.swift
+++ ParseStarterProject/WaitingForMembersViewController.swift
@@ -9,24 +9,23 @@
 import Foundation
 import Parse
 
-class WaitingForMembersViewController: UIViewController {
+class WaitingForMembersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var timer: NSTimer?
     var usernames = [String]();
 
     @IBOutlet weak var joinedMemberCount: UILabel!
     @IBOutlet weak var groupCode: UILabel!
+    @IBOutlet weak var tableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        groupCode.text = Meal.curMeal?.groupCode
         //ever two seconds check server to see if other users have joined
         getUsernames()
-        groupCode.text = Meal.curMeal?.groupCode
         timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("getUsernames"), userInfo: nil, repeats: true)
-
-        print("hi", usernames)
     }
-
+    
     //updates usernames variable with list of people
     func getUsernames(){
         print("running getUsernames")
@@ -41,6 +40,17 @@ class WaitingForMembersViewController: UIViewController {
         }
         joinedMemberCount.text = String(usernames.count) + " member(s) have joined your party"
         print(usernames)
+        tableview.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usernames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("usernameCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = usernames[indexPath.item] == User.curUser?.username ? usernames[indexPath.item] + " (you)" : usernames[indexPath.item]
+        return cell
     }
     
 }
